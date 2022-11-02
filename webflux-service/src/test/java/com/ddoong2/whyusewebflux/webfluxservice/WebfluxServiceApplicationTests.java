8468573b02e5355ebcb38c9bba70d1fb59bd37bb
고대준 <daejoon.ko@hanatour.com>
2022-11-02 17:51:50 +0900

    테스트 메소드명 한글로 변경

--- webflux-service/src/test/java/com/ddoong2/whyusewebflux/webfluxservice/WebfluxServiceApplicationTests.java
+++ webflux-service/src/test/java/com/ddoong2/whyusewebflux/webfluxservice/WebfluxServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ddoong2.whyusewebflux.webfluxservice;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 class WebfluxServiceApplicationTests {
 
     @Test
-    void contextLoads() {
+    @DisplayName("환경을 로드한다")
+    void 환경을_로드한다() {
+
+        // Given - 사전 조건 설정
+        // When - 검증하려는 로직 실행
+        // Then - 출력 확인
     }
 
 }
