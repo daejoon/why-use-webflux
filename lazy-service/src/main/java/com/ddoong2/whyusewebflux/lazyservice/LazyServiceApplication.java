5044942c1aa213f577b6d915a084814f664a4325
고대준 <daejoon.ko@hanatour.com>
2022-11-02 17:03:12 +0900

    Lazy API 작성

--- lazy-service/src/main/java/com/ddoong2/whyusewebflux/lazyservice/LazyServiceApplication.java
+++ lazy-service/src/main/java/com/ddoong2/whyusewebflux/lazyservice/LazyServiceApplication.java
@@ -1,7 +1,11 @@
 package com.ddoong2.whyusewebflux.lazyservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LazyServiceApplication {
@@ -10,4 +14,20 @@ public class LazyServiceApplication {
         SpringApplication.run(LazyServiceApplication.class, args);
     }
 
+
+    @Slf4j
+    @RestController
+    public static class LazyController {
+
+        @GetMapping("/wait/{time}")
+        public String wait(@PathVariable Integer time) {
+            try {
+                Thread.sleep(time.longValue());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return String.format("%d millisecond", time.longValue());
+        }
+
+    }
 }
