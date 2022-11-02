c3c7bac313501d3e98fad891b268352a52e19451
고대준 <daejoon.ko@hanatour.com>
2022-11-02 17:14:53 +0900

    Lazy API 변경

--- lazy-service/src/main/java/com/ddoong2/whyusewebflux/lazyservice/LazyServiceApplication.java
+++ lazy-service/src/main/java/com/ddoong2/whyusewebflux/lazyservice/LazyServiceApplication.java
@@ -1,6 +1,8 @@
 package com.ddoong2.whyusewebflux.lazyservice;
 
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,19 +16,33 @@ public class LazyServiceApplication {
         SpringApplication.run(LazyServiceApplication.class, args);
     }
 
+    @Getter
+    @Setter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    @Builder
+    public static class LazyMessage {
+        private String message;
+    }
+
 
     @Slf4j
     @RestController
     public static class LazyController {
 
+        @Value("${spring.application.name}")
+        private String applicationName;
+
         @GetMapping("/wait/{time}")
-        public String wait(@PathVariable Integer time) {
+        public LazyMessage wait(@PathVariable Long time) {
             try {
                 Thread.sleep(time.longValue());
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            return String.format("%d millisecond", time.longValue());
+            return LazyMessage.builder()
+                    .message(String.format("%s - %d millisecond", applicationName, time.longValue()))
+                    .build();
         }
 
     }
